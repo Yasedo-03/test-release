95d5ffc9ecdeb0acc776a7d64e8905dfcf721c89
ADRIEN LEBORGNE <81620287+Yasedo-03@users.noreply.github.com>
2023-01-16 17:27:26 +0100

    fix(namerc.js): fix regex

--- .validate-branch-namerc.js
+++ .validate-branch-namerc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    pattern: '^(feat|fix|docs|perf|refactor)-[0-9]{6}-.+$',
+    pattern: '^(feat|fix|docs|perf|refactor)/[0-9]{6}/.+$',
     errorMsg:
-      '🤨 La branche que tu essaies de pusher ne respecte pas nos conventions => "<(feat|fix|docs|perf|refactor)>-<numéro du ticket>-<short-description>", tu peux la renommer avec `git branch -m <nom-actuel> <nouveau-nom>`',
+      '🤨 La branche que tu essaies de pusher ne respecte pas nos conventions => "<(feat|fix|docs|perf|refactor)>/<numéro du ticket>/<short-description>", tu peux la renommer avec `git branch -m <nom-actuel> <nouveau-nom>`',
 }
